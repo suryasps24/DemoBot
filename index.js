fe8cd380be551ed9551b52e1cd52ac3d1fd9145a
Surya Punj Sharma <suryapunjsharma@gmail.com>
2022-04-01 13:01:26 +0530

    Try1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { webhookrouter, testBot } = require("./route/route");
 app.use(express.json());
 app.use(webhookrouter);
 app.use(testBot); // front end
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server is running on port ", port); //Server listening at mentioned Port.
 });
